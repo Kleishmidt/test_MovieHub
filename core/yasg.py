932fc831ae9b70add34f369f89756ba75d92ef34
Kleishmidt <kleishmidt.dmytro@gmail.com>
2024-02-27 14:48:03 +0200

    Update yasg title and add filters in installed apps

--- core/yasg.py
+++ core/yasg.py
@@ -5,7 +5,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Django Structure",
+        title="MovieHub API",
         default_version='v1',
     ),
     public=True,
